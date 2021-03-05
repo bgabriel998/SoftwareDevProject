0e353536282da7ea7cb9f1cfb9531be717c8581a
Maximilian Gangloff <maximilian.gangloff@epfl.ch>
2021-03-05 16:38:37 +0100

    Added buttons to MainActivity

---
+++ code/app/src/main/java/com/github/bgabriel998/softwaredevproject/Button7Activity.java
@@ -0,0 +1,14 @@
+package com.github.bgabriel998.softwaredevproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Button7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button7);
+    }
+}
