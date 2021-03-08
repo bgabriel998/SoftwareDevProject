a8d54ced2ce43bf645bae5819cae19acad52479d
Maximilian Gangloff <maximilian.gangloff@epfl.ch>
2021-03-08 16:12:20 +0100

    Added camera-preview

---
+++ code/app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/CameraActivityTest.java
@@ -0,0 +1,5 @@
+package com.github.bgabriel998.softwaredevproject;
+
+public class CameraActivityTest {
+    
+}
--- code/app/src/main/java/com/github/bgabriel998/softwaredevproject/MainActivity.java
+++ code/app/src/main/java/com/github/bgabriel998/softwaredevproject/MainActivity.java
@@ -1,12 +1,19 @@
 package com.github.bgabriel998.softwaredevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST_CODE = 100;
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +22,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void button1(View view) {
-        Intent intent = new Intent(this, Button1Activity.class);
-        startActivity(intent);
+        if(hasCameraPermission()){
+            Intent intent = new Intent(this, Button1Activity.class);
+            startActivity(intent);
+        }
+        else{
+            requestCameraPermission();
+            if(hasCameraPermission()){
+                Intent intent = new Intent(this, Button1Activity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
     }
 
     public void button2(View view) {
