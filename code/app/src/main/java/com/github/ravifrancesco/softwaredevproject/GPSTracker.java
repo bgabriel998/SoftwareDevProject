c2409876f4469a4fa4120f577fdb70b54c3f2497
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-09 20:36:05 +0100

    Added GPS tracker GUI

--- code/app/src/main/java/com/github/ravifrancesco/softwaredevproject/GPSTracker.java
+++ code/app/src/main/java/com/github/ravifrancesco/softwaredevproject/GPSTracker.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,8 +12,10 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.util.Log;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 
 public class GPSTracker extends Service implements LocationListener {
@@ -158,6 +161,36 @@ public class GPSTracker extends Service implements LocationListener {
         return getAccuracy();
     }
 
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    // shows alert in case of wrong settings
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alertDialog.show();
+    }
 
     @Override
     public void onLocationChanged(Location location) {
