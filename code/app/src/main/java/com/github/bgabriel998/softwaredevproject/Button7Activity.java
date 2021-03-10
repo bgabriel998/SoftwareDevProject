6fcdcadf894aa92f43d30f12abb2a0e494a260e3
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-10 15:42:36 +0100

    Moved GPSTrackerActivity.java to Button7Activity.java

--- code/app/src/main/java/com/github/bgabriel998/softwaredevproject/Button7Activity.java
+++ code/app/src/main/java/com/github/bgabriel998/softwaredevproject/Button7Activity.java
@@ -1,14 +1,72 @@
 package com.github.bgabriel998.softwaredevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Button7Activity extends AppCompatActivity {
+import com.github.ravifrancesco.softwaredevproject.GPSTracker;
+
+public class Button7Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private GPSTracker gpsTracker;
+
+    TextView latitudeTV;
+    TextView longitudeTV;
+    TextView altitudeTV;
+    TextView accuracyTV;
+
+    Button getLocationButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button7);
+
+        latitudeTV = findViewById(R.id.LatitudePlainText);
+        longitudeTV = findViewById(R.id.LongitudePlainText);
+        altitudeTV = findViewById(R.id.AltitudePlainText);
+        accuracyTV = findViewById(R.id.AccuracyPlainText);
+
+        getLocationButton = (Button)findViewById(R.id.GetLocationButton);
+
+        getLocationButton.setOnClickListener(this);
+
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        gpsTracker = new GPSTracker(Button7Activity.this);
+
+    }
+
+    private void getLocation(View view){
+        if(gpsTracker.canGetLocation()){
+            double latitude = gpsTracker.getLatitude();
+            double longitude = gpsTracker.getLongitude();
+            double altitude = gpsTracker.getAltitude();
+            double accuracy = gpsTracker.getAccuracy();
+            latitudeTV.setText(String.valueOf(latitude));
+            longitudeTV.setText(String.valueOf(longitude));
+            altitudeTV.setText(String.valueOf(altitude));
+            accuracyTV.setText(String.valueOf(accuracy));
+        }else{
+            gpsTracker.showSettingsAlert();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        getLocation(view);
     }
 }
