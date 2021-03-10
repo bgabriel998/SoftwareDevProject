d666d55556b9a187bdbbee738adb327c053db081
Maximilian Gangloff <maximilian.gangloff@epfl.ch>
2021-03-10 18:51:46 +0100

    Added test for clicking on button1

--- code/app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/CameraActivityTest.java
+++ code/app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/CameraActivityTest.java
@@ -1,5 +1,15 @@
 package com.github.bgabriel998.softwaredevproject;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class CameraActivityTest {
-    
+    @Rule
+    public ActivityScenarioRule<Button1Activity> testRule = new ActivityScenarioRule<>(Button1Activity.class);
+
 }
