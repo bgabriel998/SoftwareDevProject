f03c8ec3a0dcdaaa8a3c3c00416a52a901363944
bgabriel998 <bastien.gabriel@epfl.ch>
2021-03-16 14:15:27 +0100

    add method calls to Geonames handler for testing purposes

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/Button4Activity.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/Button4Activity.java
@@ -2,72 +2,44 @@ package com.github.bgabriel998.softwaredevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.util.Log;
 
 import com.github.ravifrancesco.softwaredevproject.UserPoint;
 
 import org.osmdroid.bonuspack.location.POI;
 import org.osmdroid.util.GeoPoint;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class Button4Activity extends AppCompatActivity {
-
-    private TextView positionTextView;
-    private TextView resultsTextView;
-    private Button getSurroundingPeaks;
-    private UserPoint userPoint;
-    private double latitude = 0.0F;
-    private double longitude = 0.0F;
-    private double altitude = 0.0F;
-    private int accuracy = 0;
-    private GeonamesHandler handler;
-
+    long start;
+    long end;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button4);
-        positionTextView = findViewById(R.id.positionTextView);
-        resultsTextView = findViewById(R.id.querryResultTextView);
-        getSurroundingPeaks = findViewById(R.id.button9);
 
-        userPoint = new UserPoint(this);
-        handler = new GeonamesHandler("bgabrie1");
-        getLocation();
+        UserPoint userLocation = new UserPoint(this);
+        userLocation.update();
 
-        getSurroundingPeaks.setOnClickListener(new View.OnClickListener() {
+        GeonamesHandler handler = new GeonamesHandler(userLocation,20,4000,40) {
             @Override
-            public void onClick(View v) {
-                getLocation();
-                handler.startGetSurroundingPeaks(new GeoPoint(latitude,longitude),200,10);
-                try {
-                    ArrayList<POI> results = handler.getSurroundingPeaksResult();
-                    StringBuilder sb = new StringBuilder();
-                    for(POI point : results){
-                        sb.append("Name: "+point.mType +" Alt: "+point.mLocation.getAltitude()+"\n");
-                    }
-                    resultsTextView.setText(sb.toString());
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    onClick(v);
-                }
+            public void onResponseReceived(Object result) {
+                end = System.currentTimeMillis();
+                Log.v("GEONAMES","elapsed "+(end-start));
+                ArrayList<POI> pois = (ArrayList<POI>) result;
 
-
+                for(POI point : pois)
+                    Log.v("GEONAMES","Name: "+point.mType + "alt: "+point.mLocation.getAltitude());
+                Log.v("GEONAMES","size: "+pois.size());
             }
-        });
-    }
+        };
 
-    private void getLocation(){
-        latitude = 45.91070396589361;
-        longitude =  6.933626866680844;
-        altitude = 1035;
-        accuracy = 10;
-        String positionStr = "Lat: "+latitude + " Long: "+longitude
-                +" Alt: "+altitude+" Accuracy: "+accuracy+" m";
-        positionTextView.setText(positionStr);
+        Log.v("GEONAMES","Start Get");
+        start = System.currentTimeMillis();
+        handler.execute();
+
     }
 }
