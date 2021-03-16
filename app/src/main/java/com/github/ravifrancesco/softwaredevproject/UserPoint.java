e9fa73e7181346cd50c3659f1253495d30e4e6df
bgabriel998 <bastien.gabriel@epfl.ch>
2021-03-16 14:14:52 +0100

    Add computeBounding box function to compute a area around the user with given radius

--- app/src/main/java/com/github/ravifrancesco/softwaredevproject/UserPoint.java
+++ app/src/main/java/com/github/ravifrancesco/softwaredevproject/UserPoint.java
@@ -5,10 +5,14 @@ import android.util.Log;
 
 import com.google.gson.internal.$Gson$Preconditions;
 
+import org.osmdroid.util.BoundingBox;
+
 import java.util.Observable;
 import java.util.Observer;
 
 public class UserPoint extends Point {
+
+    private final double ADJUST_COORDINATES = 0.008983112; // 1km in degrees at equator.
 
     private GPSTracker gpsTracker;
 
@@ -18,6 +22,7 @@ public class UserPoint extends Point {
         super(0, 0, 0);
         gpsTracker = new GPSTracker(mContext, this);
     }
+
 
     public void update() {
         super.setLatitude(gpsTracker.getLatitude());
@@ -33,5 +38,21 @@ public class UserPoint extends Point {
     public boolean canGetLocation() {
         return gpsTracker.canGetLocation();
     }
+
+
+    /**
+     * Computes bounding box around
+     * @param rangeInKm range around user point to compute the bounding box (in km)
+     * @return Bounding box around user point
+     */
+    public BoundingBox computeBoundingBox(double rangeInKm){
+        double north = super.getLatitude() + ( rangeInKm * ADJUST_COORDINATES);
+        double south = super.getLatitude() - ( rangeInKm * ADJUST_COORDINATES);
+        double lngRatio = 1/Math.cos(Math.toRadians(super.getLatitude()));
+        double east = super.getLongitude() + (rangeInKm * ADJUST_COORDINATES) * lngRatio;
+        double west = super.getLongitude() - (rangeInKm * ADJUST_COORDINATES) * lngRatio;
+        return new BoundingBox(north,east,south,west);
+    }
+
 
 }
