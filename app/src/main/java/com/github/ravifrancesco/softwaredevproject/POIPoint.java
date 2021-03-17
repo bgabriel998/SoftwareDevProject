a6a79cfc443c02bb03c767c7ef630e150f6ac0e7
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-03-17 12:51:15 +0100

    Added Doc for POIPoint.java

--- app/src/main/java/com/github/ravifrancesco/softwaredevproject/POIPoint.java
+++ app/src/main/java/com/github/ravifrancesco/softwaredevproject/POIPoint.java
@@ -2,9 +2,18 @@ package com.github.ravifrancesco.softwaredevproject;
 
 import org.osmdroid.util.GeoPoint;
 
+/**
+ * POIPoint is a class that extends Point.java and represents a POI in the map.
+ *
+ * It provides a constructor to build a POIPoint from a org.osmdroid.util.GeoPoint
+ */
 public class POIPoint extends Point {
 
-    // construct a POIPoint given a org.osmdroid.util.GeoPoint
+    /**
+     * Constructor for POIPoint
+     *
+     * @param geoPoint GEOPoint from org.osmdroid.util.GeoPoint
+     */
     public POIPoint(GeoPoint geoPoint) {
         super(geoPoint.getLatitude(), geoPoint.getLongitude(), geoPoint.getAltitude());
     }
