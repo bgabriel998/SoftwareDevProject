be8bc110860364770962df5dc8700d8ee30dc90f
bgabriel998 <bastien.gabriel@epfl.ch>
2021-03-19 11:35:16 +0100

    remove imports

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/Button4Activity.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/Button4Activity.java
@@ -1,27 +1,8 @@
 package com.github.bgabriel998.softwaredevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
 
-import com.github.ravifrancesco.softwaredevproject.UserPoint;
-import com.google.android.gms.location.FusedLocationProviderClient;
-
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
-import org.osmdroid.bonuspack.location.POI;
-import org.osmdroid.util.GeoPoint;
-
-import java.util.ArrayList;
 
 public class Button4Activity extends AppCompatActivity {
 
