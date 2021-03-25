f227d5f87316a39715f4d66b7cf981cac81a8570
Giovanni Monea <giovanni.monea@epfl.ch>
2021-03-25 23:07:29 +0100

    Added tests for username choice and changed toasts with snackbars

--- app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/ProfileActivityTest.java
+++ app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/ProfileActivityTest.java
@@ -1,19 +1,27 @@
 package com.github.bgabriel998.softwaredevproject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.IBinder;
+import android.view.WindowManager;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.Root;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
@@ -23,6 +31,26 @@ import static org.junit.Assert.assertSame;
 
 @RunWith(AndroidJUnit4.class)
 public class ProfileActivityTest {
+
+    /* auxiliary class to check toasts */
+    public class ToastMatcher extends TypeSafeMatcher<Root> {
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("is toast");
+        }
+
+        @Override
+        public boolean matchesSafely(Root root) {
+            int type = root.getWindowLayoutParams().get().type;
+            if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+                IBinder windowToken = root.getDecorView().getWindowToken();
+                IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                if (windowToken == appToken) return true;
+            }
+            return false;
+        }
+    }
 
     @Rule
     public ActivityScenarioRule<ProfileActivity> testRule = new ActivityScenarioRule<>(ProfileActivity.class);
@@ -58,5 +86,62 @@ public class ProfileActivityTest {
         String CHANGE_USERNAME = "Insert your username here";
         ViewInteraction changeUsernameText = Espresso.onView(withId(R.id.editTextUsername));
         changeUsernameText.check(matches(withHint(CHANGE_USERNAME)));
+    }
+
+    /* Test that the username choice UI is correct */
+    @Test
+    public void ChangeUsernameUITest() {
+        testRule.getScenario().onActivity(ProfileActivity::setUsernameChoiceUI);
+        Espresso.onView(withId(R.id.signInButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        Espresso.onView(withId(R.id.signOutButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        Espresso.onView(withId(R.id.changeUsernameButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        Espresso.onView(withId(R.id.submitUsernameButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        Espresso.onView(withId(R.id.editTextUsername)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
+    /* Test that if the username is already present the correct message is displayed */
+    @Test
+    public void TestUsernameAlreadyPresent() throws InterruptedException {
+        final String usedUsername = "usernameTest";
+        final String errorMessage = "The username " + usedUsername + " is already used by another user. Choose a new one.";
+        testRule.getScenario().onActivity(ProfileActivity::setUsernameChoiceUI);
+        onView(withId(R.id.editTextUsername)).perform(typeText(usedUsername));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.submitUsernameButton)).perform(click());
+        Thread.sleep(500);
+        onView(withText(errorMessage)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        Thread.sleep(500);
+        Espresso.onView(withId(R.id.signInButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+    }
+
+    /* Test that if the username has changed the correct message is displayed */
+    /* No account is created with this test as rules will not allow it */
+    @Test
+    public void TestRegisterUser() throws InterruptedException {
+        final String username = "i3gn4u39n4t34o";
+        final String message = "Your username has changed!";
+        testRule.getScenario().onActivity(ProfileActivity::setUsernameChoiceUI);
+        onView(withId(R.id.editTextUsername)).perform(typeText(username));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.submitUsernameButton)).perform(click());
+        Thread.sleep(500);
+        onView(withText(message)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        Thread.sleep(500);
+        Espresso.onView(withId(R.id.signInButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
+    /* Test that if the username chooses his current username the correct message is displayed */
+    @Test
+    public void TestChosenCurrentUsername() throws InterruptedException {
+        final String username = "null";
+        final String message = "You can't choose the username you already have!";
+        testRule.getScenario().onActivity(ProfileActivity::setUsernameChoiceUI);
+        onView(withId(R.id.editTextUsername)).perform(typeText(username));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.submitUsernameButton)).perform(click());
+        Thread.sleep(500);
+        onView(withText(message)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        Thread.sleep(500);
+        Espresso.onView(withId(R.id.signInButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
     }
 }
