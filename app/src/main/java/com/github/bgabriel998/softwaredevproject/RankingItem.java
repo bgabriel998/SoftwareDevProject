c5c2ab8a3ae99e56fdd76fec11393160be3529e6
Alexander Olsson <aleol711@student.liu.se>
2021-03-28 18:17:17 +0200

    Removed uncollected items

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/RankingItem.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/RankingItem.java
@@ -17,10 +17,4 @@ public class RankingItem {
         this.username = username;
         this.points = points;
     }
-
-    @Override
-    public boolean equals( Object mob2) {
-        return this.username.equals( ((RankingItem) mob2).username) &&
-                this.points == ((RankingItem) mob2).points;
-    }
 }
