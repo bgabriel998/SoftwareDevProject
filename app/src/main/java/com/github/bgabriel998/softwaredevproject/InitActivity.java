240e58cd4ae5562dccac71eaa70123dea05a259f
bgabriel998 <bastien.gabriel@epfl.ch>
2021-04-01 19:01:51 +0200

    Add new pre-loading activity and made it the first activity

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/InitActivity.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/InitActivity.java
@@ -2,7 +2,13 @@ package com.github.bgabriel998.softwaredevproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.github.giommok.softwaredevproject.FirebaseAccount;
 
 public class InitActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class InitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
+
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        initApp();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
+    private void initApp() {
+        FirebaseAccount firebaseAccount = FirebaseAccount.getAccount();
+
+        firebaseAccount.synchronizeDiscoveredCountryHighPoints();
+        firebaseAccount.synchronizeUserScore();
+        firebaseAccount.synchronizeUsername();
+
+
     }
 }
