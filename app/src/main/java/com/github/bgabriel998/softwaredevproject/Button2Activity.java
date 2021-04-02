38b6738dba7e48ff78318bc0d6e822e8ddd8de53
bgabriel998 <bastien.gabriel@epfl.ch>
2021-04-02 15:56:49 +0200

    cleanup button2 activity

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/Button2Activity.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/Button2Activity.java
@@ -33,35 +33,5 @@ public class Button2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button2);
-
-
-        FirebaseAccount firebaseAccount = FirebaseAccount.getAccount();
-        UserScore userScore = new UserScore(this, firebaseAccount);
-
-
-        GeoPoint geoPoint_1 = new GeoPoint(45.8325,6.8641666666667,4810);
-        POIPoint point_1 = new POIPoint(geoPoint_1);
-        point_1.setName("Mont Blanc - Monte Bianco");
-
-        GeoPoint geoPoint_2 = new GeoPoint(45.86355980599387, 6.951348205683087,4013);
-        POIPoint point_2 = new POIPoint(geoPoint_2);
-        point_2.setName("Dent du Geant");
-
-        GeoPoint geoPoint_3 = new GeoPoint(45.891667, 6.907222,3673);
-        POIPoint point_3 = new POIPoint(geoPoint_3);
-        point_3.setName("Aiguille du plan");
-
-        GeoPoint geoPoint_4 = new GeoPoint(47.421111, 10.985278,2962);
-        POIPoint point_4 = new POIPoint(geoPoint_4);
-        point_4.setName("Zugspitze");
-
-        ArrayList<POIPoint> inputArrayList = new ArrayList<POIPoint>();
-        //inputArrayList.add(point_2);
-        //inputArrayList.add(point_1);
-        //inputArrayList.add(point_3);
-        inputArrayList.add(point_4);
-
-        userScore.updateUserScore(inputArrayList);
-
     }
 }
