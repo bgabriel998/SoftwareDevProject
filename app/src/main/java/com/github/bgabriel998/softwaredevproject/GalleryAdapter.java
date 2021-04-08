27b07b7fdd82418fcc42c169a32a33d2621ff299
aolsson711 <79709825+aolsson711@users.noreply.github.com>
2021-04-08 09:46:48 +0200

    Update app/src/main/java/com/github/bgabriel998/softwaredevproject/GalleryAdapter.java
    Co-authored-by: magangloff <71378343+magangloff@users.noreply.github.com>

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/GalleryAdapter.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/GalleryAdapter.java
@@ -77,7 +77,7 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
      * Contains the image view to be able to set the correct image.
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView image;
+        private final ImageView image;
 
         /**
          * Constructor to get the image view.
