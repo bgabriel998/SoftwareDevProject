df1f66690b4d83c0dd076d7841ac8e0c316d34d5
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-04-10 18:39:29 +0200

    Added .doc for LineOfSight.java

--- app/src/main/java/com/github/ravifrancesco/softwaredevproject/LineOfSight.java
+++ app/src/main/java/com/github/ravifrancesco/softwaredevproject/LineOfSight.java
@@ -6,6 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * This class enables the computation of the visible POIPoints from the user's location.
+ * It uses the ElevationMap class to compute the elevation of the terrain at a given location.
+ *
+ * The ELEVATION_DIFFERENCE_THRESHOLD represents the maximum acceptable difference in meters
+ * between the line that connects the user to the POIPoint and the actual elevation of the
+ * terrain in a given point.
+ */
 public class LineOfSight {
 
     static final int ELEVATION_DIFFERENCE_THRESHOLD = 50; // in meters
@@ -16,12 +24,25 @@ public class LineOfSight {
     private double mapCellSize;
     private double boundingBoxWestLon;
 
+    /**
+     * Constructor for the LineOfSight class.
+     *
+     * @param userPoint userPoint from wich the visible POIPoints are computed.
+     */
     public LineOfSight(UserPoint userPoint) {
         this.userPoint = userPoint;
         this.elevationMap = new ElevationMap(this.userPoint);
     }
 
+    /**
+     * This method allows to filter the POIPoints that are not visible from the user's location.
+     *
+     * @param poiPoints a List of POIPoint to filter.
+     * @return          a list of the visible POIPoints from the input list.
+     */
     public List<POIPoint> getVisiblePoints(List<POIPoint> poiPoints) {
+
+        elevationMap.updateElevationMatrix();
 
         this.mapCellSize = elevationMap.getMapCellSize();
         this.boundingBoxWestLon = elevationMap.getBoundingBoxWestLong();
@@ -37,6 +58,17 @@ public class LineOfSight {
 
     }
 
+    /**
+     * This method computes a boolean that indicates if a single POIPoint is visible from
+     * the user's location.
+     *
+     * @param poiPoint      POIPoint to determine if it is visible.
+     * @param userIndexes   indexes representing the user's location on the elevation map grid.
+     * @param userLongitude longitude of the user's location (in degrees).
+     * @param userAltitude  altitude of the user's location (in meters).
+     * @return              <code>true</code> if the POIPoint is visible from the user's location.
+     * 	                    <code>false</code> otherwise.
+     */
     private boolean isVisible(POIPoint poiPoint, Pair<Integer, Integer> userIndexes,
                               double userLongitude, int userAltitude) {
 
@@ -52,12 +84,24 @@ public class LineOfSight {
 
          return line.parallelStream()
                 .map(p -> elevationMap.getAltitudeAtLocation(p.first, p.second) -
-                        computeMaxAltitude(userLongitude, userAltitude, p.second, slope) >
+                        computeMaxElevation(userLongitude, userAltitude, p.second, slope) >
                         ELEVATION_DIFFERENCE_THRESHOLD)
                 .reduce(true, (vis, v) -> vis &&  v);
 
     }
 
+    /**
+     * This method draws the line between the user's location and the POIPoint on the
+     * elevation map grid. To do this the Bresenham algorithm is used:
+     *
+     * See <a href="Bresenham's line algorithm">https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm</a>
+     *
+     * @param x1    x index of the user's location on the elevation map grid.
+     * @param y1    y index of the user's location on the elevation map grid.
+     * @param x2    x index of the POIPoint on the elevation map grid.
+     * @param y2    y index of the POIPoint on the elevation map grid.
+     * @return      a List of indexes representing the line in the elevation map grid.
+     */
     private List<Pair<Integer, Integer>> bresenham(int x1, int y1, int x2, int y2) {
 
         int m_new = 2 * (y2 - y1);
@@ -85,10 +129,20 @@ public class LineOfSight {
 
     }
 
-    private int computeMaxAltitude(double userLongitude, double userAltitude,
-                                   int rowIndex, double slope) {
+    /**
+     * Method that computes the elevation of the line connecting the user's location and the
+     * POIPoint at a given index of the elevation map grid.
+     *
+     * @param userLongitude longitude of the user's location (in degrees).
+     * @param userAltitude  altitude of the user's location (in meters).
+     * @param colIndex      index of the column to compute the elevation.
+     * @param slope         slope of the line conncetgint the user's location and the POIPoint.
+     * @return              elevation (in meters).
+     */
+    private int computeMaxElevation(double userLongitude, double userAltitude,
+                                    int colIndex, double slope) {
 
-        double longitude = rowIndex * mapCellSize + boundingBoxWestLon;
+        double longitude = colIndex * mapCellSize + boundingBoxWestLon;
 
         return (int) (slope*(longitude - userLongitude) + userAltitude);
 
