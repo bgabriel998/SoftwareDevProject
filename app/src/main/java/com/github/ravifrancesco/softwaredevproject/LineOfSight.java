032d159f3ab7c65b00cdb4d727093da4c775cd05
Ravi Francesco Srinivasan <57806748+ravifrancesco@users.noreply.github.com>
2021-04-10 19:00:44 +0200

    Update app/src/main/java/com/github/ravifrancesco/softwaredevproject/LineOfSight.java
    Co-authored-by: magangloff <71378343+magangloff@users.noreply.github.com>

--- app/src/main/java/com/github/ravifrancesco/softwaredevproject/LineOfSight.java
+++ app/src/main/java/com/github/ravifrancesco/softwaredevproject/LineOfSight.java
@@ -118,8 +118,7 @@ public class LineOfSight {
 
             // Slope error reached limit, time to
             // increment y and update slope error.
-            if (slope_error_new >= 0)
-            {
+            if (slope_error_new >= 0){
                 y++;
                 slope_error_new -= 2 * (x2 - x1);
             }
