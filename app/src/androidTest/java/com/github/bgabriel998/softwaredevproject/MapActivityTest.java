c17a19d8c29f3013f5cca3ea8b23a498fe34c382
bgabriel998 <bastien.gabriel@epfl.ch>
2021-04-23 09:41:08 +0200

    remove openclose test

--- app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/MapActivityTest.java
+++ app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/MapActivityTest.java
@@ -1,33 +1,12 @@
 package com.github.bgabriel998.softwaredevproject;
 
-import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MapActivityTest {
 
-
-    @Test
-    public void checkOpenClose(){
-        Intents.init();
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Intents.release();
-    }
-
+    
 }
