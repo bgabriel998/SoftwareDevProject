1023a685ec8f45aa97cab0ece534af9c037c39a1
Alexander Olsson <aleol711@student.liu.se>
2021-04-26 14:29:44 +0200

    Created fullscreen

--- app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/gallery/ImageActivityTest.java
+++ app/src/androidTest/java/com/github/bgabriel998/softwaredevproject/gallery/ImageActivityTest.java
@@ -33,7 +33,6 @@ import static org.junit.Assert.fail;
 @RunWith(AndroidJUnit4.class)
 public class ImageActivityTest {
 
-    private static final int testImage = R.drawable.temp_diablerets;
     private static final String imagePath = CameraActivity.getOutputDirectory(ApplicationProvider.getApplicationContext()) +
                                                 "/TestImage";
 
@@ -57,27 +56,6 @@ public class ImageActivityTest {
 
     @Rule
     public ActivityScenarioRule<ImageActivity> testRule = new ActivityScenarioRule<>(intent);
-
-    /* Test that the toolbar title is set as expected */
-    @Test
-    public void TestToolbarTitle(){
-        String TOOLBAR_TITLE = "Image";
-        ViewInteraction greetingText = Espresso.onView(withId(R.id.toolbarTitle));
-        greetingText.check(matches(withText(TOOLBAR_TITLE)));
-    }
-
-    /* Test that the activity finishes when the toolbar back button is pressed. */
-    @Test
-    public void TestToolbarBackButton(){
-        onView(withId(R.id.toolbarBackButton)).perform(click());
-        try {
-            Thread.sleep(1000);
-            assertSame(testRule.getScenario().getResult().getResultCode(), Activity.RESULT_CANCELED);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            fail("TestToolbarBackButton failed");
-        }
-    }
 
     /* Test that the image view has the same image as the intent is. */
     @Test
