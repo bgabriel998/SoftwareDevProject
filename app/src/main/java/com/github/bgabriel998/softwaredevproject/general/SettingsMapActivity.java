38b01b4320e4dd647ce51dd27030d95bc9000c21
Ravi Francesco Srinivasan <57806748+ravifrancesco@users.noreply.github.com>
2021-04-30 10:27:15 +0200

    Update app/src/main/java/com/github/bgabriel998/softwaredevproject/general/SettingsMapActivity.java
    Co-authored-by: magangloff <71378343+magangloff@users.noreply.github.com>

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/general/SettingsMapActivity.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/general/SettingsMapActivity.java
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
