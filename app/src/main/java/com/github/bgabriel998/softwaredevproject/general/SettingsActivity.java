ce941efdef629fb4637b91c81992a1b40eb709e9
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-04-30 11:23:20 +0200

    Added Offline Mode Button to settings for Demo

--- app/src/main/java/com/github/bgabriel998/softwaredevproject/general/SettingsActivity.java
+++ app/src/main/java/com/github/bgabriel998/softwaredevproject/general/SettingsActivity.java
@@ -2,9 +2,12 @@ package com.github.bgabriel998.softwaredevproject.general;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.github.bgabriel998.softwaredevproject.R;
+import com.github.bgabriel998.softwaredevproject.collection.CollectionActivity;
 import com.github.bgabriel998.softwaredevproject.utils.ToolbarHandler;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -16,5 +19,11 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         ToolbarHandler.SetupToolbar(this, TOOLBAR_TITLE);
+    }
+
+    /** Changes view to SettingsMapActivity */
+    public void offlineModeButton(View view) {
+        Intent intent = new Intent(this, SettingsMapActivity.class);
+        startActivity(intent);
     }
 }
