222da1f7ce23a921fe65a9d532c89dfd799443a9
giommok <giovanni.monea@epfl.ch>
2021-05-02 19:06:21 +0200

    Implemented RemoteResource IF in FirebaseFriendItem

--- app/src/main/java/ch/epfl/sdp/peakar/user/services/providers/firebase/FirebaseFriendItem.java
+++ app/src/main/java/ch/epfl/sdp/peakar/user/services/providers/firebase/FirebaseFriendItem.java
@@ -8,10 +8,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
 import ch.epfl.sdp.peakar.database.Database;
+import ch.epfl.sdp.peakar.general.remote.RemoteOutcome;
+import ch.epfl.sdp.peakar.general.remote.RemoteResource;
 import ch.epfl.sdp.peakar.user.friends.FriendItem;
 
-public class FirebaseFriendItem extends FriendItem {
-    private final DatabaseReference dbRef;
+public class FirebaseFriendItem extends FriendItem implements RemoteResource {
+    private DatabaseReference dbRef;
     private ValueEventListener itemListener;
 
     /**
@@ -21,26 +23,7 @@ public class FirebaseFriendItem extends FriendItem {
      */
     public FirebaseFriendItem(String uid) {
         super(uid);
-
-        dbRef = Database.refRoot.child(Database.CHILD_USERS).child(uid);
-
-        // Define the listener
-        itemListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Integer newPoints = snapshot.child(Database.CHILD_SCORE).getValue(Integer.class);
-                String newUsername = snapshot.child(Database.CHILD_USERNAME).getValue(String.class);
-                if(newPoints != null) setPoints(newPoints);
-                if(newUsername != null) setUsername(newUsername);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        };
-
-        // Add the listener
-        dbRef.addValueEventListener(itemListener);
+        retrieveData();
     }
 
     /**
@@ -56,5 +39,32 @@ public class FirebaseFriendItem extends FriendItem {
      */
     public void removeListener() {
         dbRef.removeEventListener(itemListener);
+    }
+
+    @Override
+    public RemoteOutcome retrieveData() {
+        dbRef = Database.refRoot.child(Database.CHILD_USERS).child(super.getUid());
+
+        // Create the listener
+        itemListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Integer newPoints = snapshot.child(Database.CHILD_SCORE).getValue(Integer.class);
+                String newUsername = snapshot.child(Database.CHILD_USERNAME).getValue(String.class);
+                if(newPoints != null) setPoints(newPoints);
+                if(newUsername != null) setUsername(newUsername);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        };
+        return null;
+    }
+
+    @Override
+    public void loadData() {
+        // Add the listener that will load data whenever something changes
+        dbRef.addValueEventListener(itemListener);
     }
 }
