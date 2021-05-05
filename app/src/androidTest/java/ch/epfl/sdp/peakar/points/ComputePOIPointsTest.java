130f4ea47578672dc2cd1910fcb137c57b240ce5
ravifrancesco <ravi.srinivasan99@gmail.com>
2021-05-05 22:11:56 +0200

    Added tests

--- app/src/androidTest/java/ch/epfl/sdp/peakar/points/ComputePOIPointsTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/peakar/points/ComputePOIPointsTest.java
@@ -2,16 +2,34 @@ package ch.epfl.sdp.peakar.points;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 
+import androidx.core.util.Pair;
+import androidx.preference.PreferenceManager;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.GrantPermissionRule;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.osmdroid.util.BoundingBox;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.epfl.sdp.peakar.R;
+import ch.epfl.sdp.peakar.general.SettingsMapActivity;
+import ch.epfl.sdp.peakar.utils.OfflineContentContainer;
 
 import static java.lang.Double.NaN;
 import static org.junit.Assert.assertEquals;
@@ -106,5 +124,74 @@ public class ComputePOIPointsTest {
         //Check that POIPoints and labeledPOIPoints are not null
         assertNotNull(ComputePOIPoints.POIPoints);
         assertNotNull(ComputePOIPoints.labeledPOIPoints);
+    }
+
+    /* Test if the offline content is loaded correctly */
+    @Test
+    public void loadPOIsFromFileTest() {
+
+        new ComputePOIPoints(mContext);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+
+        // Activate offline mode
+        // Offline mode not activated, reset shared preference
+        prefs.edit()
+                .putBoolean(mContext.getResources().getString(R.string.offline_mode_key), true)
+                .apply();
+
+
+
+        BoundingBox mBoundingBox = userPoint.computeBoundingBox(GeonamesHandler.DEFAULT_RANGE_IN_KM);
+        System.out.println(mBoundingBox.getCenterWithDateLine());
+
+        List<POIPoint> mPoiPoints = new ArrayList<>();
+        POIPoint pointOne = new POIPoint("pointOne", 45.802540, 6.850330, 3000);
+        POIPoint pointTwo = new POIPoint("pointTwo", 45.802536, 6.850326, 5000);
+        mPoiPoints.add(pointOne);
+        mPoiPoints.add(pointTwo);
+
+        int[][] mElevationMap = {{0,1}, {2,3}};
+        Pair<int[][], Double> mTopography =  new Pair<>(mElevationMap,  0.1);
+
+        OfflineContentContainer offlineContentContainer = new OfflineContentContainer();
+        offlineContentContainer.topography = mTopography;
+        offlineContentContainer.boundingBox = mBoundingBox;
+        offlineContentContainer.POIPoints = mPoiPoints;
+
+        saveJson(offlineContentContainer);
+
+        userPoint.update();
+
+        List<POIPoint> loadedPOIPoints =  ComputePOIPoints.POIPoints;
+
+        Assert.assertEquals(pointOne, loadedPOIPoints.get(0));
+        Assert.assertEquals(pointTwo, loadedPOIPoints.get(1));
+
+        userPoint.setLocation(0, 0, 0, 0);
+
+        userPoint.update();
+
+        loadedPOIPoints = ComputePOIPoints.POIPoints;
+
+        Assert.assertTrue(loadedPOIPoints.isEmpty());
+
+        // Reset location
+        userPoint.setLocation(45.802537, 6.850328, 4809, 0);
+
+    }
+
+    /* Helper method to save Json */
+    private void saveJson(OfflineContentContainer saveObject) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonString = gson.toJson(saveObject);
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(mContext.openFileOutput(SettingsMapActivity.OFFLINE_CONTENT_FILE, Context.MODE_PRIVATE));
+            outputStreamWriter.write(jsonString);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 }
