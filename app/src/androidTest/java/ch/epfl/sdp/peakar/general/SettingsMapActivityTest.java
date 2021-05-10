61fb02d62f944470d47543b53fdb03e9777c2c7f
Ravi Francesco Srinivasan <57806748+ravifrancesco@users.noreply.github.com>
2021-05-10 17:14:28 +0200

    Update app/src/androidTest/java/ch/epfl/sdp/peakar/general/SettingsMapActivityTest.java
    Co-authored-by: magangloff <71378343+magangloff@users.noreply.github.com>

--- app/src/androidTest/java/ch/epfl/sdp/peakar/general/SettingsMapActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/peakar/general/SettingsMapActivityTest.java
@@ -27,7 +27,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 
 import ch.epfl.sdp.peakar.R;
 
