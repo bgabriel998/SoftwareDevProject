dda1e2a8de3a0c0ec912a0914ef47d9345813bd6
bgabriel998 <bastien.gabriel@epfl.ch>
2021-05-13 11:41:15 +0200

    change callback for SettingsButton

--- app/src/main/java/ch/epfl/sdp/peakar/general/MainMenuActivity.java
+++ app/src/main/java/ch/epfl/sdp/peakar/general/MainMenuActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -28,6 +29,9 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        ImageButton button = (ImageButton) findViewById(R.id.settingsButton);
+        button.setOnClickListener(this::settingsButton);
     }
 
     /** Changes view to SettingsActivity */
