74cf40c792b221e03e3ffca6a425179d8f01ff5a
magangloff <71378343+magangloff@users.noreply.github.com>
2021-05-16 19:31:15 +0200

    Created util class for the computation of the POIPoints

--- app/src/main/java/ch/epfl/sdp/peakar/points/ComputePOIPoints.java
+++ app/src/main/java/ch/epfl/sdp/peakar/points/ComputePOIPoints.java
@@ -19,18 +19,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.stream.Collectors;
 
 import ch.epfl.sdp.peakar.R;
 import ch.epfl.sdp.peakar.general.SettingsMapActivity;
 import ch.epfl.sdp.peakar.utils.OfflineContentContainer;
+
+import static ch.epfl.sdp.peakar.utils.POIPointsUtilities.filterHighestPOIs;
 
 /**
  * Requests the POIPoints around the user location and then downloads the topography map once the
@@ -50,8 +48,6 @@ public class ComputePOIPoints extends Observable implements Observer{
 
     private static final int MAX_LOADING_DISTANCE = 20000; // in m
 
-    private static final int HALF_MARKER_SIZE_WIDTH = 3;
-    private static final int HALF_MARKER_SIZE_HEIGHT = 5;
 
     @SuppressLint("StaticFieldLeak")
     private static ComputePOIPoints single_instance = null; // singleton instance
@@ -413,71 +409,6 @@ public class ComputePOIPoints extends Observable implements Observer{
      */
     public Map<POIPoint, Boolean> getFilteredPOIsOutOfSight(){
         return isLineOfSightAvailable() ? filteredLabeledPOIsOutOfSight : filteredPOIPoints;
-    }
-
-    /**
-     * Filters the labeled POIPoints and takes only the highest s.t. there are no other markers
-     * within 6° horizontally. POIPoints in the line of sight are prioritised. If a POIPoint is in
-     * the line of sight but has a smaller altitude then another POIPoint that is close but that is
-     * not in the line of sight, then only the POIPoint that is in the line of sight will be added
-     * @param labeledPOIPoints labeled POIPoints that get sorted
-     * @return Map<POIPoint, Boolean> of the highest filtered POIs
-     */
-    Map<POIPoint, Boolean> filterHighestPOIs(Map<POIPoint, Boolean> labeledPOIPoints) {
-        //sort the labeledPOIPoints by altitude in decreasing order
-        Map<POIPoint, Boolean> sortedPoisByAlt = labeledPOIPoints.entrySet().stream()
-                .sorted(Collections.reverseOrder(Comparator.comparingDouble(p -> p.getKey().altitude)))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-
-        //New map that contains only the filtered POIPoints
-        Map<POIPoint, Boolean> filteredPOIs = new HashMap<>();
-
-        //Iterate over all POIPoints in labeledPOIPoints
-        for (Map.Entry<POIPoint, Boolean> poiPoint : sortedPoisByAlt.entrySet()) {
-            if(comparePoiToFiltered(poiPoint, filteredPOIs)){
-                filteredPOIs.put(poiPoint.getKey(), poiPoint.getValue());
-            }
-        }
-        return filteredPOIs;
-    }
-
-    /**
-     * Compares a entry of a Map<POIPoint, Boolean> to the Map of filteredPOIs to decide if
-     * the POIPoint will be added or not to the filteredPOIs Map.
-     * @param poiPoint Entry of a Map<POIPoint, Boolean> of the POIPoints that needs to be filtered
-     * @param filteredPOIs Map<POIPoint, Boolean> that contains the filtered POIPOints
-     * @return True if the POIPoint should be added, false otherwise
-     */
-    private boolean comparePoiToFiltered(Map.Entry<POIPoint, Boolean> poiPoint, Map<POIPoint, Boolean> filteredPOIs) {
-        //Iterate over all mountains in the filtered Map
-        for (Map.Entry<POIPoint, Boolean> resPoiPoints : filteredPOIs.entrySet()) {
-            //If the difference between one of the filtered POIPoints and the actual POIPoint
-            //is less than HALF_MARKER_SIZE then do no add the marker
-            if(Math.abs(poiPoint.getKey().getHorizontalBearing()
-                    - resPoiPoints.getKey().getHorizontalBearing()) <= HALF_MARKER_SIZE_WIDTH){
-
-                //Break if the markers are not close vertically
-                if(Math.abs(poiPoint.getKey().getVerticalBearing()
-                        - resPoiPoints.getKey().getVerticalBearing()) >= HALF_MARKER_SIZE_HEIGHT){
-                    return true;
-                }
-
-                //If the POIPoint in the filtered Map is in the line of sight or the actual
-                //POIPoint is not in the line of sight
-                if(resPoiPoints.getValue() || !poiPoint.getValue()){
-                    //Set addPoiPoint to false
-                    return false;
-                }
-                //If the actual mountain is in the line of sight but the filtered is not
-                else{
-                    //Then remove the filtered
-                    filteredPOIs.remove(resPoiPoints.getKey());
-                    return true;
-                }
-            }
-        }
-        return true;
     }
 
     @Override
