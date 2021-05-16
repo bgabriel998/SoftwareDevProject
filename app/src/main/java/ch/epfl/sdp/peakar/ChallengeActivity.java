c7e1ebb58659281a3c5b30ecd4262a29baf4b760
bgabriel998 <bastien.gabriel@epfl.ch>
2021-05-16 16:11:55 +0200

    add testing activity for challenge creation

---
+++ app/src/main/java/ch/epfl/sdp/peakar/ChallengeActivity.java
@@ -0,0 +1,14 @@
+package ch.epfl.sdp.peakar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChallengeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+    }
+}
