f658d082dc92ec8b694683265607c6b5f102f0d7
giommok <79704208+giommok@users.noreply.github.com>
2021-05-18 13:49:42 +0200

    Update app/src/main/java/ch/epfl/sdp/peakar/user/profile/NewProfileActivity.java
    Co-authored-by: magangloff <71378343+magangloff@users.noreply.github.com>

--- app/src/main/java/ch/epfl/sdp/peakar/user/profile/NewProfileActivity.java
+++ app/src/main/java/ch/epfl/sdp/peakar/user/profile/NewProfileActivity.java
@@ -269,7 +269,22 @@ public class NewProfileActivity extends AppCompatActivity {
         };
         changeThread.start();
     }
+    /**
+     * Hides the keyboard and updates the UI after the username was changed or cancelled only if the
+     * user is already registered
+     */
+    private void setUiUsernameChanged() {
+        if (getCurrentFocus() != null && !AuthService.getInstance().getAuthAccount().getUsername().equals(Account.USERNAME_BEFORE_REGISTRATION)) {
+            // Hide username edit
+            findViewById(R.id.profile_username_edit).setVisibility(View.GONE);
 
+            // Show username
+            findViewById(R.id.profile_username).setVisibility(View.VISIBLE);
+
+            // Show change button
+            findViewById(R.id.profile_change_username).setVisibility(View.VISIBLE);
+        }
+    }
     /**
      * Method to hide the keyboard
      */
