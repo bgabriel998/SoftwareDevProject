949a7427ebb80ae818d48b969bcd905b60a28893
giommok <giovanni.monea@epfl.ch>
2021-05-18 10:24:35 +0200

    Added test for collected items sorting

--- app/src/androidTest/java/ch/epfl/sdp/peakar/user/NewProfileActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/peakar/user/NewProfileActivityTest.java
@@ -3,8 +3,10 @@ package ch.epfl.sdp.peakar.user;
 import android.content.Intent;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.widget.ListView;
 
 import androidx.lifecycle.Lifecycle;
+import androidx.test.espresso.DataInteraction;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
@@ -18,17 +20,26 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.osmdroid.util.GeoPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import ch.epfl.sdp.peakar.R;
+import ch.epfl.sdp.peakar.collection.NewCollectedItem;
 import ch.epfl.sdp.peakar.database.Database;
+import ch.epfl.sdp.peakar.points.POIPoint;
+import ch.epfl.sdp.peakar.rankings.RankingItem;
 import ch.epfl.sdp.peakar.user.profile.NewProfileActivity;
+import ch.epfl.sdp.peakar.user.score.UserScore;
 import ch.epfl.sdp.peakar.user.services.AuthAccount;
 import ch.epfl.sdp.peakar.user.services.AuthService;
 import ch.epfl.sdp.peakar.user.services.FirebaseAuthService;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
@@ -38,8 +49,26 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sdp.peakar.database.DatabaseTest.databaseRefRoot;
 import static ch.epfl.sdp.peakar.user.AuthAccountTest.registerAuthUser;
 import static ch.epfl.sdp.peakar.user.AuthAccountTest.removeAuthUser;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.AIGUILLE_DU_PLAN_ALT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.AIGUILLE_DU_PLAN_LAT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.AIGUILLE_DU_PLAN_LONG;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.AIGUILLE_DU_PLAN_NAME;
 import static ch.epfl.sdp.peakar.utils.TestingConstants.BASIC_USERNAME;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.DENT_DU_GEANT_ALT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.DENT_DU_GEANT_LAT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.DENT_DU_GEANT_LONG;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.DENT_DU_GEANT_NAME;
 import static ch.epfl.sdp.peakar.utils.TestingConstants.LONG_SLEEP_TIME;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.MONT_BLANC_ALT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.MONT_BLANC_LAT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.MONT_BLANC_LONG;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.MONT_BLANC_NAME;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.POINTE_DE_LAPAZ_ALT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.POINTE_DE_LAPAZ_LAT;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.POINTE_DE_LAPAZ_LONG;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.POINTE_DE_LAPAZ_NAME;
+import static ch.epfl.sdp.peakar.utils.TestingConstants.THREAD_SLEEP_5S;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertNotSame;
 
 public class NewProfileActivityTest {
@@ -217,5 +246,61 @@ public class NewProfileActivityTest {
         onView(withId(R.id.profile_picture)).perform(click());
         Espresso.onView(withId(R.id.profile_username)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
         Espresso.onView(withId(R.id.profile_username_edit)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
+    /* Test that collected mountains are sorted according to their points */
+    @Test
+    public void sortedByPoints_CollectedItemsTest() {
+        // Prepare the account
+        databaseRefRoot.child(Database.CHILD_USERS).child(AuthService.getInstance().getID()).child(Database.CHILD_USERNAME).setValue(user1);
+        UserScore userScore = new UserScore(InstrumentationRegistry.getInstrumentation().getTargetContext());
+
+        GeoPoint geoPoint_1 = new GeoPoint(MONT_BLANC_LAT,MONT_BLANC_LONG,MONT_BLANC_ALT);
+        POIPoint point_1 = new POIPoint(geoPoint_1);
+        point_1.setName(MONT_BLANC_NAME);
+
+        GeoPoint geoPoint_2 = new GeoPoint(DENT_DU_GEANT_LAT, DENT_DU_GEANT_LONG,DENT_DU_GEANT_ALT);
+        POIPoint point_2 = new POIPoint(geoPoint_2);
+        point_2.setName(DENT_DU_GEANT_NAME);
+
+        GeoPoint geoPoint_3 = new GeoPoint(AIGUILLE_DU_PLAN_LAT, AIGUILLE_DU_PLAN_LONG,AIGUILLE_DU_PLAN_ALT);
+        POIPoint point_3 = new POIPoint(geoPoint_3);
+        point_3.setName(AIGUILLE_DU_PLAN_NAME);
+
+        GeoPoint geoPoint_4 = new GeoPoint(POINTE_DE_LAPAZ_LAT, POINTE_DE_LAPAZ_LONG,POINTE_DE_LAPAZ_ALT);
+        POIPoint point_4 = new POIPoint(geoPoint_4);
+        point_4.setName(POINTE_DE_LAPAZ_NAME);
+
+        ArrayList<POIPoint> inputArrayList = new ArrayList<POIPoint>();
+        inputArrayList.add(point_1);
+        inputArrayList.add(point_2);
+        inputArrayList.add(point_3);
+        inputArrayList.add(point_4);
+
+        userScore.updateUserScoreAndDiscoveredPeaks(inputArrayList);
+
+        // Update the account
+        FirebaseAuthService.getInstance().forceRetrieveData();
+        testRule.getScenario().recreate();
+
+        try {
+            Thread.sleep(THREAD_SLEEP_5S);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Check correct order
+        DataInteraction interaction = onData(instanceOf(NewCollectedItem.class));
+
+        try {
+            Thread.sleep(THREAD_SLEEP_5S);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        interaction.atPosition(0).onChildView(withId(R.id.collected_name)).check(matches((withText(MONT_BLANC_NAME))));
+        interaction.atPosition(1).onChildView(withId(R.id.collected_name)).check(matches((withText(DENT_DU_GEANT_NAME))));
+        interaction.atPosition(2).onChildView(withId(R.id.collected_name)).check(matches((withText(AIGUILLE_DU_PLAN_NAME))));
+        interaction.atPosition(3).onChildView(withId(R.id.collected_name)).check(matches((withText(POINTE_DE_LAPAZ_NAME))));
     }
 }
