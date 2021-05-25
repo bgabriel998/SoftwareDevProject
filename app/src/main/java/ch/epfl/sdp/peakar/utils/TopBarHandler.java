64f8e8539662ba45d7aec4a901534654a2a22b6c
aolsson711 <79709825+aolsson711@users.noreply.github.com>
2021-05-25 13:11:30 +0200

    Update app/src/main/java/ch/epfl/sdp/peakar/utils/TopBarHandler.java
    Co-authored-by: magangloff <71378343+magangloff@users.noreply.github.com>

--- app/src/main/java/ch/epfl/sdp/peakar/utils/TopBarHandler.java
+++ app/src/main/java/ch/epfl/sdp/peakar/utils/TopBarHandler.java
@@ -3,7 +3,6 @@ package ch.epfl.sdp.peakar.utils;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 
